104ee12e47b5b0d5af599c9eed064a7b6f2c83ef
Winlyn Liu <wl1698@nyu.edu>
2019-04-06 19:41:25 -0400

    Door/doorframe model, started raycast script

---
+++ TheStanleyProject/Assets/Scripts/Raycast.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public float rayDistance = 1f;
+    public Camera player;
+
+    void Update()
+    {
+        Ray myRay = new Ray(transform.position, transform.forward);
+
+        Debug.DrawRay(myRay.origin, myRay.direction * rayDistance, Color.blue);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            if (hit.collider.CompareTag("insert tag here") && Input.GetKeyDown(KeyCode.E))
+            {
+                //interact script
+            }
+        }
+    }
+}
