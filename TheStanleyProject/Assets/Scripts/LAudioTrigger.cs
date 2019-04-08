7db201f51acf55fc644bbbb4a96fd4a25a3b5439
Luke Brockmann <lb3297@nyu.edu>
2019-04-08 17:56:06 -0400

    -Added basic audio trigger in main scene. -Added tag on stanley called "Stanley" -Added audio source on Stanley for narration audio clips -Added mp3 file

---
+++ TheStanleyProject/Assets/Scripts/LAudioTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LAudioTrigger : MonoBehaviour
+{
+
+    public GameObject Stanley;
+
+    public AudioClip What;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Stanley")
+        {
+            Stanley.GetComponent<AudioSource>().PlayOneShot(What);
+            Debug.Log("play: " + Stanley.GetComponent<AudioSource>().clip + Time.time);
+            Destroy(this.gameObject);
+        }
+    }
+}
