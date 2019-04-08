909ed4a3d034cfd0bd4d23d7f73590ba6a7860f1
K8 <kah703@nyu.edu>
2019-04-07 20:14:30 -0400

    finished door open/close script  /// added my test scene

---
+++ TheStanleyProject/Assets/Scripts/OpenDoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorTrigger : MonoBehaviour
+{
+    public GameObject doorHingeToOpen;
+    private DoorManager doorManagerScript;
+
+    void Start()
+    {
+        doorManagerScript = doorHingeToOpen.GetComponent<DoorManager>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       doorManagerScript.openDoor();
+    }
+}
