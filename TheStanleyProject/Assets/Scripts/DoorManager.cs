ec4d75274280a94e3cf69609e8588090473bdd97
K8 <kah703@nyu.edu>
2019-04-15 21:06:38 -0400

    now are 3 bools on a door, startOpen, turnedRight, and turnedLeft

--- TheStanleyProject/Assets/Scripts/DoorManager.cs
+++ TheStanleyProject/Assets/Scripts/DoorManager.cs
@@ -4,49 +4,93 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    public bool opening = false;
-    public bool closing = false;
-    public bool startOpen = false;
-    public Vector3 rotationAxis = new Vector3(0,1,0);
-    private float rotationSpeed = 1.0f;
-    private float currentRotation;
-    private Quaternion cRotation;
-
-    void Start()
-    {
-        if (startOpen)
+    private bool opening = false;
+        private bool closing = false;
+        public bool startOpen = false;
+        private bool print = false;
+        public bool turnedRight = false;
+        public bool turnedLeft = false;
+        private Vector3 rotationAxis = new Vector3(0,1,0);
+        private float rotationSpeed = 1;
+        private float currentRotation;
+        private Quaternion cRotation;
+    
+        void Start()
         {
-            opening = true;
+            if (startOpen)
+            {
+                opening = true;
+            }
+    
+            if (turnedRight || turnedLeft)
+            {
+                rotationAxis.y = -1;
+                rotationSpeed = rotationSpeed * -1;
+            }
         }
-    }
-
-    void Update()
-    {
-        //check
-        
-        cRotation = transform.rotation;
-        currentRotation = cRotation.eulerAngles.y;
-        //print(currentRotation);
-        if (currentRotation > 0 && currentRotation < 270)
+    
+        void Update()
         {
-            opening = false;
+            //check
+            
+            
+            
+            cRotation = transform.rotation;
+            currentRotation = cRotation.eulerAngles.y;
+            if (print)
+            {
+                print(currentRotation);
+            }
+    
+            if (rotationAxis.y > 0)
+            {
+                if (currentRotation > 0 && currentRotation < 270)
+                {
+                    opening = false;
+                }
+    
+                if (currentRotation < 269)
+                {
+                    closing = false;
+                }
+            }
+    
+            if (turnedRight)
+            {
+                if (currentRotation > 90)
+                {
+                    opening = false;
+                }
+    
+                if (currentRotation > 89 && currentRotation < 270)
+                {
+                    closing = false;
+                }
+            }
+
+            if (turnedLeft)
+            {
+                if (currentRotation < 180)
+                {
+                    opening = false;
+                }
+    
+                if (currentRotation > 268)
+                {
+                    closing = false;
+                }
+            }
+            //move
+            if (opening)
+            {
+                transform.Rotate(rotationAxis,-rotationSpeed);
+            }else if (closing)
+            {
+                transform.Rotate(rotationAxis,rotationSpeed);
+            }
+    
+    
         }
-
-        if (currentRotation < 269)
-        {
-            closing = false;
-        }
-        //move
-        if (opening)
-        {
-            transform.Rotate(rotationAxis,-rotationSpeed);
-        }else if (closing)
-        {
-            transform.Rotate(rotationAxis,rotationSpeed);
-        }
-
-
-    }
 
     public void openDoor()
     {
