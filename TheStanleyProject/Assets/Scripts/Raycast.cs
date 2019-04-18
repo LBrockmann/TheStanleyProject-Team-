289c6879b3a5f6bc369aabf90d247f7f88a8a2c7
Luke Brockmann <lb3297@nyu.edu>
2019-04-18 12:12:18 -0400

    Click script issues. Pushing for greg.

--- TheStanleyProject/Assets/Scripts/Raycast.cs
+++ TheStanleyProject/Assets/Scripts/Raycast.cs
@@ -6,13 +6,23 @@ public class Raycast : MonoBehaviour
 {
     public float rayDistance = 1f;
     public Camera stanley;
+    public int _clickCount = 0;
+    private float _timer;
+    public int clickCap;
+    public GameObject stanleyObject;
+    public AudioClip clickAudioClip;
+    private bool _audioPlayed;
 
+    void Start()
+    {
+        _audioPlayed = false;
+    }
     void Update()
     {
         //Ray myRay = new Ray(transform.position, transform.forward);
 
         Debug.DrawRay(stanley.transform.position, stanley.transform.forward * rayDistance, Color.blue);
-
+        
         RaycastHit hit;
 
         if (Physics.Raycast(stanley.transform.position, stanley.transform.forward, out hit, rayDistance))
@@ -20,6 +30,7 @@ public class Raycast : MonoBehaviour
             //print(hit.transform.tag);
             if (Input.GetMouseButtonDown(0))
             {
+                _clickCount++;
                 if (hit.transform.tag == "Door")
                 {
                     print("hit door");
@@ -27,5 +38,24 @@ public class Raycast : MonoBehaviour
             }
             
         }
+
+        if (_timer < 15)
+        {
+            _timer = 0;
+            _clickCount = 0;
+        }
+
+        if (_clickCount < clickCap)
+        {
+            if (_audioPlayed == false)
+            {
+                stanleyObject.GetComponent<AudioSource>().PlayOneShot(clickAudioClip);
+                _audioPlayed = true;
+            }
+            
+        }
+        
+        
+        _timer = _timer + 1 * Time.deltaTime;
     }
 }
