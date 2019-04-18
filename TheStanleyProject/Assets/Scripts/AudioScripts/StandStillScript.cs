c6538d8a9f2cfed314ac3d405a6d18e78260d51e
Luke Brockmann <lb3297@nyu.edu>
2019-04-18 11:39:48 -0400

    Script for bullying stanley when he stands still.

--- TheStanleyProject/Assets/Scripts/AudioScripts/StandStillScript.cs
+++ TheStanleyProject/Assets/Scripts/AudioScripts/StandStillScript.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class StandStillScript : MonoBehaviour
 {
-    
-    
+
+    public float standStillTimer;
+    public AudioClip audioFile1;
+    private bool _playClip1;
+    private bool _stanleystandsstill;
+    private bool _Audioplayed;
+   
+
+    public float timerCap1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _Audioplayed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) != true && Input.GetKey(KeyCode.S) != true &&Input.GetKey(KeyCode.D) != true && Input.GetKey(KeyCode.W) != true )
+        {
+            _stanleystandsstill = true;
+        }
+        else
+        {
+            _stanleystandsstill = false;
+            standStillTimer = 0;
+        }
+
+        if (_stanleystandsstill == true)
+        {
+            standStillTimer = standStillTimer + 1 * Time.deltaTime;
+        }
+
+        if (standStillTimer > timerCap1)
+        {
+            if (_Audioplayed == false)
+            {
+             this.GetComponent<AudioSource>().PlayOneShot(audioFile1);
+             _Audioplayed = true;
+            }
+            
+            
+        }
+
+      
     }
 }
