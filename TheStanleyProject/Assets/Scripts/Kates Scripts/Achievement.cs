548e558908c0808dfe5ac97d5e2d142f446584d4
K8 <kah703@nyu.edu>
2019-04-20 20:32:18 -0400

    Achievement script working and Pause screen navigation code added

--- TheStanleyProject/Assets/Scripts/Kates Scripts/Achievement.cs
+++ TheStanleyProject/Assets/Scripts/Kates Scripts/Achievement.cs
@@ -6,47 +6,52 @@ using UnityEngine.UI;
 public class Achievement: MonoBehaviour
 {
     public Image achievementNoti;
+    public Text bigText;
+    public Text smallTest;
 
     public Sprite placeHolder;
+    public Sprite pic1;
+    public Sprite pic2;
 
-    public int notiTimer = 100;
+    public int notiTimer = 1000;
     public int timer = 0;
+
+    public Raycast rC;
     
     public class Achievements
     {
         public bool achieved;
-        public Sprite notiPicture;
         public string notiText;
+        public string notiSmallText;
         public int activation;
 
-        public Achievements(string noti, Sprite notiP)
+        public int picNumber;
+
+        public Achievements(string noti, string noti2, int pic)
         {
             achieved = false;
-            notiPicture = notiP;
             notiText = noti;
             activation = 0;
+            notiSmallText = noti2;
+
+            picNumber = pic;
+
 
         }
 
-        public void Activate()
-        {
-            //achievementNoti.gameObject.SetActive(true);
-            //achievementNoti.sprite = notiPicture;
-            achieved = true;
-        }
     }
 
-    //public Achievements jump = new Achievements("Jump 3 Times", placeHolder);
-    //public Achievements random = new Achievements("Youre Lucky", placeHolder);
+    public Achievements jump = new Achievements("You cant jump","no seriously we never added that",1);
+    public Achievements random = new Achievements("Youre Lucky", "you did nothing to get this",2);
 
 
     private void Start()
     {
         achievementNoti.gameObject.SetActive(false);
-        int i = Random.Range(0, 1);
+        float i = Random.Range(0.0f, 1.0f);
         if (i > .5)
         {
-           // random.Activate();
+           Activate(random);
         }
         
     }
@@ -66,21 +71,49 @@ public class Achievement: MonoBehaviour
         //jump achievement
         if (Input.GetKeyDown(KeyCode.Space))
         {
-           // if (!jump.achieved)
-            //{
-           //     jump.activation += 1; 
-           // }
+            if (!jump.achieved)
+            {
+                jump.activation += 1; 
+            }
         }
 
-        //if (!jump.achieved && jump.activation > 2)
-        //{
-        //    jump.Activate();
-       // }
+        if (!jump.achieved && jump.activation > 2)
+        {
+            Activate(jump);
+        }
     }
 
     void Reset()
     {
-        //achievementNoti.gameObject.SetActive(false);
+        achievementNoti.gameObject.SetActive(false);
+        bigText.gameObject.SetActive(false);
+        smallTest.gameObject.SetActive(false);
+    }
+
+    public void Activate(Achievements a)
+    {
+        timer = 0;
+        achievementNoti.gameObject.SetActive(true);
+        bigText.gameObject.SetActive(true);
+        smallTest.gameObject.SetActive(true);
+
+        if (a.picNumber == 1)
+        {
+            achievementNoti.sprite = pic1;
+        }if (a.picNumber == 2)
+        {
+            achievementNoti.sprite = pic2;
+        }
+        else
+        {
+            achievementNoti.sprite = placeHolder;
+        }
+
+        bigText.text = a.notiText;
+        smallTest.text = a.notiSmallText;
+
+        a.achieved = true;
+
     }
    
     
---
+++ TheStanleyProject/Assets/Scripts/Kates Scripts/PauseScreen.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public bool paused;
+
+    void Start()
+    {
+        pauseScreen.SetActive(false);
+        paused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                UnPause();
+            }
+            else
+            {
+                Pause(); 
+            }
+            
+            
+        }
+    }
+
+    public void Pause()
+    {
+        pauseScreen.SetActive(true);
+        paused = true;
+    }
+
+    public void UnPause()
+    {
+        pauseScreen.SetActive(false);
+        paused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MasterOffice");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
